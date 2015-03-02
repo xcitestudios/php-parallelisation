1f71e7e69ba83f19a36c79e274782ec333ed3c03
Wade Womersley <wade85@gmail.com>
2015-03-02 15:30:29 +0000

    Added phpdoc build with summaries in files.

--- src/Interfaces/EventInterface.php
+++ src/Interfaces/EventInterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * com.xcitestudios.Parallelisation
+ *
+ * @copyright Wade Womersley (xcitestudios)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @link https://xcitestudios.com/
+ */
+
 namespace com\xcitestudios\Parallelisation\Interfaces;
 
 use com\xcitestudios\Generic\Data\Manipulation\Interfaces\SerializationInterface;
